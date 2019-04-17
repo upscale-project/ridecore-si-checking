193c417b18650d29f77e14e09ea0a61771a3a28d
Florian Lonsing <lonsing@cs.stanford.edu>
2019-04-16 17:41:53 -0700

    worked on testing workflow

--- testing-workflow/run-si-tests.sh
+++ testing-workflow/run-si-tests.sh
@@ -3,31 +3,26 @@
 if (($# != 3))
 then
     echo "There is something wrong with the command line, expecting three parameters!"
-    echo "usage: <problem-file> <patch-dir> <verilogsrc-dir>"
-    echo "  <problem-file>   : single file containing a list of BMC problems to be solved using CoSA"
-    echo "  <patch-dir>      : directory containing patch files for bug injection. The naming scheme of"
-    echo "                     the patch files is '*_OPNAME.patch', where OPNAME the name of the operator"
-    echo "                     found in the file <problem-file>. The file '*_OPNAME.patch' is used to inject" 
-    echo "                     a bug to test the single instruction check for operator OPNAME."
-    echo "  <verilogsrc-dir> : directory containing the original Verilog source files of the design."
-    echo "                     These files will be patched to inject bugs using the patch files in <patch-dir>."
-    echo "                     After a test, 'git reset --hard' is called to undo the patch."
+    echo "usage: <problem-files-dir> <patch-dir> <verilogsrc-dir>"
+    echo "  <problem-files-dir>   : directory of files containing BMC problems to be solved using CoSA"
+    echo "  <patch-dir>           : directory containing patch files for bug injection. The naming scheme of"
+    echo "                          the patch files is '*_OPNAME.patch', where OPNAME the name of the operator"
+    echo "                          found in the file <problem-file>. The file '*_OPNAME.patch' is used to inject" 
+    echo "                          a bug to test the single instruction check for operator OPNAME."
+    echo "  <verilogsrc-dir>      : directory containing the original Verilog source files of the design."
+    echo "                          These files will be patched to inject bugs using the patch files in <patch-dir>."
+    echo "                          After a test, 'git reset --hard' is called to undo the patch."
     exit 1
 fi
 
-INPUTFILE=$1
-DIRNAME=`dirname $INPUTFILE`
+INPUTFILESDIR=$1
 PATCHDIR=$2
 VERILOGSRC=$3
-TMPFILEBMCSETUP=/tmp/tmpfilebmcsetup-$$.txt
-TMPFILEBMCTEST=$DIRNAME/tmpfilebmctest-$$.txt
 TMPFILECOSALOG=/tmp/tmpfilecosalog-$$.txt
 
 function cleanup
 {
   echo "Cleaning up temporary files." 1>&2
-  rm -f $TMPFILEBMCSETUP
-  rm -f $TMPFILEBMCTEST
   rm -f $TMPFILECOSALOG
   # function parameter indicates whether to reset
   if (($1))
@@ -43,6 +38,8 @@ trap 'cleanup "1"; exit 1' SIGHUP SIGINT SIGTERM
 # will abort to avoid losing uncommitted changes due to calls of 'git
 # reset' during this workflow.
 
+#TODO: catch newly added but not yet committed files
+
 LOCALCHANGES=`git diff | wc -l`
 if (($LOCALCHANGES))
 then
@@ -52,27 +49,18 @@ then
     exit 1
 fi
 
-# Extract default BMC setup, which will appear in every temporary
-# problem file we set up for a CoSA run
-# NOTE: extraction depends on the format of file "../cosa/single.txt"
-grep -A 3 "\[GENERAL\]" $INPUTFILE > $TMPFILEBMCSETUP
-echo "" >> $TMPFILEBMCSETUP
-grep -A 6 "\[DEFAULT\]" $INPUTFILE >> $TMPFILEBMCSETUP
-echo "" >> $TMPFILEBMCSETUP
+# Collect all CoSA problem files in INPUTFILESDIR
+declare -a INPUTFILES=(`find $INPUTFILESDIR -name "single_property_*.txt"`);
 
-# Extract the names of operators from INPUTFILE
-declare -a OPNAMES=(`grep "\[Single Instruction [fF]or" $INPUTFILE | awk '{print $4}' | awk -F] '{print $1}'`);
-
-# Extract BMC tests listed in INPUTFILE for each operator
-for OP in "${OPNAMES[@]}";
-do    
-    # Copy basic setup to new problem file
-    cat $TMPFILEBMCSETUP > $TMPFILEBMCTEST
-
+# Loop over all CoSA problem files, inject bug provided that a
+# respective patch file is available, and run CoSA on the problem file
+# to detect a counterexample.
+for INPUTFILE in "${INPUTFILES[@]}";
+do
+    echo "CoSA problem file: $INPUTFILE"
+    
+    OP=`grep "\[Single Instruction [fF]or" $INPUTFILE | awk '{print $4}' | awk -F] '{print $1}'`;
     echo "Operator name: $OP"
-
-    # Copy actual test to problem file
-    grep -A 5 "\[Single Instruction [[:alnum:]]* $OP\]" $INPUTFILE >> $TMPFILEBMCTEST
 
     # Find patch files related to operator OP, assuming its file name
     # contains the strin "$OP" (i.e., the current operator name)
@@ -101,9 +89,9 @@ do
         echo "  Patching original file $ORIGFILENAME"
         patch -i $BUGINJECTIONPATCHFILE $VERILOGSRC/$ORIGFILENAME
 
-        echo "  Running CoSA --problems $TMPFILEBMCTEST"
+        echo "  Running CoSA --problems $INPUTFILE"
         
-        CoSA --problems $TMPFILEBMCTEST 2>&1 | tee $TMPFILECOSALOG
+        CoSA --problems $INPUTFILE 2>&1 | tee $TMPFILECOSALOG
 
         grep "Result: FALSE" $TMPFILECOSALOG >/dev/null 2>&1
         RESEXPECTED=$?
